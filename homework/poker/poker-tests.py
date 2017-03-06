0118baece5a0a57cb73a6c208fada15bb5a15f67
e.tarasenko <exh@xakep.ru>
2017-03-06 21:01:55 +0300

    test_hand_raise_exception_unknown_suit_if_input_has_suit_is_non_c_h_s_d

--- homework/poker/poker-tests.py
+++ homework/poker/poker-tests.py
@@ -1,20 +1,26 @@
 import unittest
 from poker import Hand
-
+from poker import UnknownSuit
 
 class PokerTests(unittest.TestCase):
-
 
     def test_has_card_return_true_if_hand_has_ace_of_spades(self):
         hand = Hand("2C 3H 4S 8C AS")
 
-        res = hand.hasCard("AS")
+        res = hand.has_card("AS")
+
+        self.assertEqual(res, True)
+
+    def test_hand_raise_exception_unknown_suit_if_input_has_suit_is_non_c_h_s_d(self):
+        res = False
+        try:
+            Hand("2C 3W 4S 8C AS")
+        except UnknownSuit:
+            res = True
 
         self.assertEqual(res, True)
 
 if __name__ == '__main__':
     unittest.main()
-
-
 
 
--- homework/poker/poker.py
+++ homework/poker/poker.py
@@ -1,9 +1,17 @@
 
+class UnknownSuit(Exception):
+    pass
+
+
 class Hand(object):
     def __init__(self, hand):
         self.cards = set(hand.split())
 
-    def hasCard(self, card):
+        for card in self.cards:
+            if card[1] != 'S' and card[1] != 'H' and card[1] != 'D' and card[1] != 'C':
+                raise UnknownSuit
+
+    def has_card(self, card):
         return card in self.cards
 
 
